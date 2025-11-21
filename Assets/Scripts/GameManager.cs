dfd9f0271f29b8910a969f4379d535508b5a3eb4
Alex Nilsson <d25aleni@student.his.se>
2025-11-21 13:35:00 +0100

    Player Walk

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,94 @@
+using NUnit.Framework;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+GameObject bugAmountText;
+
+int numberOfBugs = 0;
+[SerializeField] private int howManyBuggsToWin = 5;
+
+public float volume = 1;
+
+[SerializeField] AudioSource musicSource;
+
+[SerializeField] Slider volumeSlider;
+
+// DialogueScript dialogueScript;
+SceneLoader sceneLoader;
+
+
+// Start is called once before the first execution of Update after the MonoBehaviour is created
+void Start()
+{
+    sceneLoader = FindObjectOfType<SceneLoader>();
+    
+    bugAmountText = GameObject.Find("BugText");
+    
+    AddBuggs(0);
+}
+
+public void StartFake()
+{
+
+    bugAmountText = GameObject.Find("BugText");
+
+    AddBuggs(0);
+    
+    
+}
+
+public void AddBuggs(int buggs)
+{
+    numberOfBugs += buggs;
+    
+    bugAmountText.GetComponent<TextMeshProUGUI>().text = numberOfBugs.ToString(); 
+
+    // if(numberOfBugs == 3)
+    // {
+    //
+    //     dialogueScript = FindObjectOfType<DialogueScript>();
+    //
+    //     dialogueScript.StartDialogue();
+    //
+    // }
+}
+
+public void CheckIfWin(bool walkedInDoor)
+{ 
+
+    if(numberOfBugs == howManyBuggsToWin)
+    {
+        if (walkedInDoor)
+        {
+            ChangeScene(2);   
+        }
+        else
+        {
+            ChangeScene(4);
+        }
+
+    }
+
+}
+
+public void ChangeScene(int whatScene)
+{
+    
+    sceneLoader.ChangeScene(whatScene);
+    
+}
+
+public void ChangeVolume()
+{
+
+     volume = volumeSlider.value;
+    
+    musicSource.volume = volume;
+
+}
+
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,148 @@
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour
+{
+
+float xInput;
+ [SerializeField] float walkSpeed = 5;
+ [SerializeField] float jumpForce = 5;
+
+ bool facingRight = true;
+
+ bool doNothing = false;
+
+ bool grounded;
+ [SerializeField] GameObject groundCheck;
+ [SerializeField] LayerMask groundMask;
+
+ Rigidbody2D myRigidbody2D;
+ Animator myAnimator;
+
+ GameManager gameManager;
+ // DialogueScript dialogueScript;
+ AudioManager audioManager;
+
+ // Start is called once before the first execution of Update after the MonoBehaviour is created
+ void Start()
+ {
+     myRigidbody2D = GetComponent<Rigidbody2D>();   
+     myAnimator = GetComponent<Animator>();
+
+     gameManager = FindObjectOfType<GameManager>();
+     // dialogueScript = FindObjectOfType<DialogueScript>();
+     audioManager = FindObjectOfType<AudioManager>();
+ }
+
+ // Update is called once per frame
+ void Update()
+ {
+     if (doNothing)
+     {
+         return;
+     }
+
+     xInput = Input.GetAxisRaw("Horizontal");
+
+
+     Vector2 inputDirection = new Vector2(xInput, 0).normalized;
+
+     //transform.localScale = new Vector2(xInput, 1);
+
+     myRigidbody2D.linearVelocity = new Vector2(inputDirection.x * walkSpeed, myRigidbody2D.linearVelocityY);
+
+     CheckGround();
+
+     if(xInput == 0 && grounded)
+     {
+
+         myAnimator.SetBool("IsRunning", false);
+
+     }
+     else
+     {
+         myAnimator.SetBool("IsRunning", true);
+     }
+
+     if (grounded && Input.GetButtonDown("Jump"))
+     {
+         myRigidbody2D.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+     }
+
+     #region Flip
+
+     if (xInput < 0 && facingRight == true)
+     {
+         Flip();
+         facingRight = false;
+     }
+
+     if (xInput > 0 && facingRight == false)
+     {
+         Flip();
+         facingRight = true;
+     }
+
+     #endregion
+
+ }
+
+ void Flip()
+ {
+
+     // V nd sprite:n genom att spegla den p  x-axeln
+     Vector3 scaler = transform.localScale;
+     scaler.x *= -1;
+     transform.localScale = scaler;
+ }
+
+ private void CheckGround()
+ {
+     grounded = Physics2D.OverlapCircle(groundCheck.transform.position, 0.35f, groundMask);
+ }
+
+ private void OnTriggerEnter2D(Collider2D collision)
+ {
+     
+     if(collision.tag == "Bugg")
+     {
+         gameManager = FindObjectOfType<GameManager>();
+         gameManager.AddBuggs(1);
+         audioManager.CollectBugSound();
+         Destroy(collision.gameObject);
+
+     }
+     
+     if(collision.tag == "Door")
+     {
+
+         gameManager.CheckIfWin(true);
+
+     }
+     
+     if(collision.tag == "BugEnjoyerDoor")
+     {
+
+         gameManager.CheckIfWin(false);
+
+     }
+
+     // if(collision.tag == "Dialogue")
+     // {
+     //
+     //     dialogueScript.StartDialogue();
+     //     Destroy(collision.gameObject);
+     //
+     // }
+
+ }
+
+ public void DoNothingTOF(bool doNothingOrNo)
+ {
+
+     doNothing = doNothingOrNo;
+
+ }
+
+}
